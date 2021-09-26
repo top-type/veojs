bb0fa9d9b40082f59a7e48e7f5eb5ad7452578d2
veoex <47295144+veoex@users.noreply.github.com>
2021-09-26 12:42:07 +0700

    send stuff

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ function buildBrowseTable() {
 		'</tr>'+
 		'</thead><tbody>';
 		trades.forEach(function(trade) {
-			var mod1 = trade.type1 == 2 ? 'NOT ' : '';
-			var mod2 = trade.type2 == 2 ? 'NOT ' : '';
+			var mod1 = trade.type1 == 2 ? '<span class="text-warning">NOT</span> ' : '';
+			var mod2 = trade.type2 == 2 ? '<span class="text-warning">NOT</span> ' : '';
 			res += '<tr class="table-active" id="' + trade.id+ '">' +
 						'<td><span class="text-success">+'+trade.amount1/1e8+'</span></td>' +
 						'<td>'+ mod1 + trade.text1.substring(0, 10)+'</td>' +
@@ -30,7 +30,7 @@ function updateBalance() {
 			html += ' <span class="text-success">(+'+u+')</span>';
 		}
 		if (u < 0) {
-			html += ' <span class="text-danger">(+'+u+')</span>';
+			html += ' <span class="text-danger">('+u+')</span>';
 		}
 		$('#walletLink').html(html);
 	})
@@ -44,6 +44,7 @@ $('#setButton').click(function(e) {
 	veo.setKeys(passphrase);
 	$('.setClass').hide();
 	$('.balance').show();
+	$('#pub').text(veo.pub().substring(0,27) + '...');
 	updateBalance();
 });
 
@@ -82,6 +83,38 @@ $('.navbar-nav>li>a').on('click', function(){
     $('.navbar-collapse').collapse('hide');
 });
 
+$('#sendButton').click(function(e) {
+	e.preventDefault();
+	var recipient = $('#recipient').val();
+	var amount = parseFloat($('#amount').val()) * 1e8;
+	veo.send(recipient, amount, function (res) {
+		updateBalance();
+	});
+});
+
+$('#maxButton').click(function(e) {
+	e.preventDefault();
+	var recipient = $('#recipient').val();
+	veo.max(recipient, function (res) {
+		$('#amount').val(res/1e8)
+	});
+});
+
+$('#copyButton').click(function(e) {
+	e.preventDefault();
+	var copyToClipboard = function(secretInfo) {
+		var $body = document.getElementsByTagName('body')[0];
+        var $tempInput = document.createElement('INPUT');
+        $body.appendChild($tempInput);
+        $tempInput.setAttribute('value', secretInfo)
+        $tempInput.select();
+        document.execCommand('copy');
+        $body.removeChild($tempInput);
+    }
+	copyToClipboard(veo.pub());
+	
+});
+
 
 
 
@@ -89,10 +122,14 @@ $(document).ready(function () {
 	if (localStorage.getItem('passphrase')) {
 		$('.balance').show();
 		veo.setKeys(localStorage.getItem('passphrase'));
+		$('#pub').text(veo.pub().substring(0,27) + '...');
 		updateBalance();
 	}
 	else {
 		$('.setClass').show();
 	}
+	setInterval(function() {
+		updateBalance();
+	}, 5000)
 	
 });
