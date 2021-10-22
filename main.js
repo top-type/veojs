4a8db6b7f6e79767192d3310ace20e6d5e6b4f87
veoex <47295144+veoex@users.noreply.github.com>
2021-10-23 05:58:08 +0700

    some qr stuff

--- main.js
+++ main.js
@@ -363,12 +363,33 @@ $('.modal').modal('show')
 
 }
 
+function onScanSuccess(decodedText, decodedResult) {
+  // handle the scanned code as you like, for example:
+  console.log(`Code matched = ${decodedText}`, decodedResult);
+}
+
+function onScanFailure(error) {
+  // handle scan failure, usually better to ignore and keep scanning.
+  // for example:
+  console.warn(`Code scan error = ${error}`);
+}
+
+let html5QrcodeScanner = new Html5QrcodeScanner(
+  "reader",
+  { fps: 10, qrbox: {width: 250, height: 250} },
+  /* verbose= */ false);
+html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+
 
 $(document).ready(function () {
 	if (localStorage.getItem('passphrase')) {
 		$('#newAccountLink').hide();
 		veo.setKeys(localStorage.getItem('passphrase'));
 		$('#pub').text(veo.pub());
+		var qr = new QRious({
+          element: document.getElementById('qr'),
+          value: veo.pub()
+        });
 		updateBalance();
 		balanceUpdater();
 		route('holdings');
